4ee94e0708f4d2a3bbbe546bae371f1063a5d9e3
Adam Gabriel <adamgabriel289@gmail.com>
2026-06-09 01:16:06 +0000

    Atualização

--- db/seed.ts
+++ db/seed.ts
@@ -3,5 +3,6 @@ import { seedMinimal } from "./seeds/seed-minimal.js";
 import { seedLogos, seedLogosAuto } from "./seeds/seed-logos.js";
 import { seedXtreinoHistorico } from "./seeds/xtreino-historico.js";
 import { seedXtreinoSchedule } from "./seeds/xtreino-schedule.js";
+import { seedXtreino08062026 } from "./seeds/seed-xtreino-08062026.js";
 
-export { seed, seedMinimal, seedLogos, seedLogosAuto, seedXtreinoHistorico, seedXtreinoSchedule };
+export { seed, seedMinimal, seedLogos, seedLogosAuto, seedXtreinoHistorico, seedXtreinoSchedule, seedXtreino08062026 };
