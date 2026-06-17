15def228da0cabd75d665a124ca9218a41bca35f
Adam Gabriel <adamgabriel289@gmail.com>
2026-06-17 01:35:47 +0000

    Atualização

--- db/seeds/seed-xtreinos.ts
+++ db/seeds/seed-xtreinos.ts
@@ -10,7 +10,7 @@ import { xtreinosRaw } from "./xtreinos-dados.js";
 import { xtreinosRawNovo } from "./xtreinos-dados-novo.js"; // 🆕
 
 // Junta tudo
-const allXtreinos = [...xtreinosRaw, ...xtreinosRawNovo];
+const allXtreinos = xtreinosRawNovo;
 
 export function seedAllXtreinos() {
   const db = getDb();
